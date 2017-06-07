f2c32eb7b13d5dad92e38036d5dcd3883d58c518
Dyas <dyas@live.com>
2017-06-07 10:51:57 +0700

    repair store

--- resources/views/upload/upload.blade.php
+++ resources/views/upload/upload.blade.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 	<h1> File Uploader <h1>
-	<form action="store" enctype="multipart/form-data" method="post">
+	<form action="/store" enctype="multipart/form-data" method="post">
 		{{ csrf_field() }}
 		<input type="file" name="image"><br>
 		<input type="submit" value="Upload">
