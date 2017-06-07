4bbb315e3843088c5066397faa4dadc54705bf89
Dyas <dyas@live.com>
2017-06-07 10:00:09 +0700

    make controller

--- app/Http/Controllers/UploadFileController.php
+++ app/Http/Controllers/UploadFileController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class UploadFileController extends Controller
 {
-    //
+	//open view
+	public function index(){
+    	return view('uploadfile');
+   	}
+
+   	//opload file
+	public function showUploadFile(Request $request){
+		$file = $request->file('image');
+
+		//Display File Name
+		echo 'File Name: '.$file->getClientOriginalName();
+		echo '<br>';
+
+		//Display File Extension
+		echo 'File Extension: '.$file->getClientOriginalExtension();
+		echo '<br>';
+
+		//Display File Real Path
+		echo 'File Real Path: '.$file->getRealPath();
+		echo '<br>';
+
+		//Display File Size
+		echo 'File Size: '.$file->getSize();
+		echo '<br>';
+
+		//Display File Mime Type
+		echo 'File Mime Type: '.$file->getMimeType();
+
+		//Move Uploaded File
+		$destinationPath = 'uploads';
+		$file->move($destinationPath,$file->getClientOriginalName());
+	}
 }
