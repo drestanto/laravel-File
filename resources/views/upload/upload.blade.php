2999f7490aecde032f2b4eacb827fe7dbffc89df
Dyas <dyas@live.com>
2017-06-07 10:32:06 +0700

    repair view

--- resources/views/upload/upload.blade.php
+++ resources/views/upload/upload.blade.php
@@ -7,6 +7,7 @@
 	<h1> File Uploader <h1>
 	<form action="" enctype="multipart/form-data" method="post">
 		<input type="file" name="image"><br>
+		<input type="submit" value="Upload">
 	</form>
 </body>
 </html>
